130e991b95bdc6ea6d8c4edfe0384892e0408d7d
Isaías Mordente <yipisaias@gmail.com>
2021-09-27 11:46:17 -0300

    adiciona menu no cliente

--- cliente.py
+++ cliente.py
@@ -1,70 +1,71 @@
 from socket import socket, AF_INET, SOCK_STREAM
-import msvcrt
 from tabuleiro import Tabuleiro
 from jogada import jogada
+from menu import menu
 
 
 msgVitoria = "Parabéns! Você venceu!"
 msgDerrota = "Seu adversário venceu!"
 
-# Create a TCP/IP socket
-clientSocket = socket(AF_INET, SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 5000)
-print('Conectando ao servidor {} na porta {}'.format(
-    server_address[0], server_address[1]))
-clientSocket.connect(server_address)
+def cliente():
+    # Create a TCP/IP socket
+    clientSocket = socket(AF_INET, SOCK_STREAM)
 
-# Cria um tabuleiro de jogo vazio
-tabuleiro = Tabuleiro()
-tabuleiro.print()
-try:
-    response = clientSocket.recv(1024).decode('utf-8')
-    if response == "2":
-        # Turno do cliente
-        jogada(tabuleiro, 'o')
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 5000)
+    print('Conectando ao servidor {} na porta {}'.format(
+        server_address[0], server_address[1]))
+    clientSocket.connect(server_address)
 
-        tabuleiro.print()
-
-        # Envia o tabuleiro para o jogador
-        clientSocket.sendall(tabuleiro.save().encode('utf-8'))
-
-    while tabuleiro.finish() == 0:
-        print("Aguardando turno do adversário...")
-
-        # Recebe a jogada do servidor
-        data = clientSocket.recv(1024)
-        if data:
-            tabuleiro.restore(data.decode('utf-8'))
-
-            print('O adversário jogou:')
-            tabuleiro.print()
-
-            # Verifica condicao de vitoria/derrota ou empate
-            if tabuleiro.result(tabuleiro.finish(), msgDerrota, msgVitoria):
-                print("Pressione qualquer tecla para sair...")
-                msvcrt.getch()
-                break
-
+    # Cria um tabuleiro de jogo vazio
+    tabuleiro = Tabuleiro()
+    tabuleiro.print()
+    try:
+        response = clientSocket.recv(1024).decode('utf-8')
+        if response == "2":
             # Turno do cliente
             jogada(tabuleiro, 'o')
 
             tabuleiro.print()
-            print('------------------')
 
-            # Envia o tabuleiro para o servidor
+            # Envia o tabuleiro para o jogador
             clientSocket.sendall(tabuleiro.save().encode('utf-8'))
 
-            # Verifica condicao de vitoria/derrota ou empate
-            if tabuleiro.result(tabuleiro.finish(), msgDerrota, msgVitoria):
-                print("Pressione qualquer tecla para sair...")
-                msvcrt.getch()
+        while tabuleiro.finish() == 0:
+            print("Aguardando turno do adversário...")
+
+            # Recebe a jogada do servidor
+            data = clientSocket.recv(1024)
+            if data:
+                tabuleiro.restore(data.decode('utf-8'))
+
+                print('O adversário jogou:')
+                tabuleiro.print()
+
+                # Verifica condicao de vitoria/derrota ou empate
+                if tabuleiro.result(tabuleiro.finish(), msgDerrota, msgVitoria):
+                    break
+
+                # Turno do cliente
+                jogada(tabuleiro, 'o')
+
+                tabuleiro.print()
+                print('------------------')
+
+                # Envia o tabuleiro para o servidor
+                clientSocket.sendall(tabuleiro.save().encode('utf-8'))
+
+                # Verifica condicao de vitoria/derrota ou empate
+                if tabuleiro.result(tabuleiro.finish(), msgDerrota, msgVitoria):
+                    break
+
+            else:
                 break
 
-        else:
-            break
+    finally:
+        print('Encerrando o cliente\n')
+        clientSocket.close()
 
-finally:
-    print('Encerrando o cliente')
-    clientSocket.close()
+
+menu(cliente)
